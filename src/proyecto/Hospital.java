f14fe44d4ee1a4fbb9ac12df76e57a4d72434700
SebaReinosoR <91642529+SebaReinosoR@users.noreply.github.com>
2021-11-14 23:48:03 -0300

    funcionamiento Funcionarios
    Se implemente alguna de los métodos claves de la clase funcionarios

--- src/proyecto/Hospital.java
+++ src/proyecto/Hospital.java
@@ -254,5 +254,101 @@ public class Hospital {
         Llamado de funciones para los funcionarios
      */
     
-   
+    public void agregarFuncionario() throws IOException {
+        String nombre, apellido, rut, especialidad;
+        int edad;
+        Funcionario nuevoFuncionario;
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < funcionarios.length; i++) {
+            if (funcionarios[i] == null) {
+                System.out.println("Ingrese su nombre:");
+                nombre = lector.readLine();
+                System.out.println("Ingrese su apellido");
+                apellido = lector.readLine();
+                System.out.println("Ingrese su rut:");
+                rut = lector.readLine();
+                System.out.println("Ingrese su edad:");
+                edad = Integer.parseInt(lector.readLine());
+                System.out.println("Ingrese el dia:");
+                especialidad = lector.readLine();
+                nuevoFuncionario = new Funcionario(especialidad, nombre, apellido, rut, edad);
+                funcionarios[i] = nuevoFuncionario;
+                System.out.println("Funcionario agregado con exito");
+            }
+        }
+
+    }
+
+    public void mostrarFuncionario() {
+        for (int i = 0; i < funcionarios.length; i++) {
+            if (funcionarios != null) {
+                funcionarios[i].gafete();
+            }
+        }
+    }
+
+    public void eliminarFuncionario(String rutFuncionario) {
+        for (int i = 0; i < funcionarios.length; i++) {
+            if (funcionarios != null && funcionarios[i].getRut().equals(rutFuncionario)) {
+                funcionarios[i] = null;
+            }
+
+        }
+    }
+
+    public void modificarFuncionario() throws IOException {
+
+        int posFuncionario = 0, indicacion = 0;
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        boolean flag = false;
+        String rutFuncionario;
+
+        System.out.println("Ingrese el rut del paciente");
+        rutFuncionario = lector.readLine();
+
+        for (int i = 0; i < funcionarios.length; i++) {
+            if (funcionarios[i].equals(rutFuncionario)) {
+                posFuncionario = i;
+                flag = true;
+            }
+        }
+
+        if (flag) {
+            do {
+                System.out.println("Que desea modificar:\n"
+                        + "1.- Nombre\n"
+                        + "2.- Apellido\n"
+                        + "3.- Edad\n"
+                        + "4.- Especialidad\n"
+                        + "0.- Salir\n");
+                indicacion = Integer.parseInt(lector.readLine());
+
+                switch (indicacion) {
+                    case 1:
+                        System.out.println("Ingrese el nombre:");
+                        funcionarios[posFuncionario].setNombres(lector.readLine());
+                        break;
+                    case 2:
+                        System.out.println("Ingrese el apellido:");
+                        funcionarios[posFuncionario].setApellidos(lector.readLine());
+                        break;
+                    case 3:
+                        System.out.println("Ingrese la edad:");
+                        funcionarios[posFuncionario].setEdad(Integer.parseInt(lector.readLine()));
+                        break;
+                    case 4:
+                        System.out.println("Ingrese la especialidad:");
+                        funcionarios[posFuncionario].setEspecialidad(lector.readLine());
+                        break;
+                    default:
+                        System.out.println("Ingrese una opcion valida");
+                }
+            } while (indicacion != 0);
+        } else {
+            System.out.println("No hay paciente con ese rut");
+        }
+
+    }
+    
+    
 }
